2bbe5daf249c739a457c80cd51ba0338ee28f36c
SamDev <47910472+FoxsDenYT@users.noreply.github.com>
2023-11-10 21:50:25 +1000

    stuff, idk 🤷🤷🤷

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let preview = document.getElementById("preview");
+let preview = document.getElementById("screen");
 let startButton = document.getElementById("screenPippy");
 let pipbtn = document.getElementById("pip");
 let recordingTimeMS = 10000;
@@ -7,6 +7,12 @@ function log(msg) {
 }
 function wait(delayInMS) {
   return new Promise(resolve => setTimeout(resolve, delayInMS));
+}
+function startWebCam(){
+ if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => preview.srcObject = stream).catch(error => log(error));
+ }
 }
 startButton.addEventListener("click", function() {
   navigator.mediaDevices.getDisplayMedia({
@@ -21,5 +27,31 @@ startButton.addEventListener("click", function() {
   .catch(log);
 }, false);
 
-preview.addEventListener("loadedmetadata", function(){preview.requestPictureInPicture()})
+preview.addEventListener("loadedmetadata", function(){
+  wait(1000)
+  preview.requestPictureInPicture()
+
+})
 pipbtn.addEventListener("click", function() {preview.requestPictureInPicture()})
+async function anySite(url){
+  document.getElementById("urlforPop").textContent = url
+  let options = {
+    width: 400,
+    height: 400,
+  };
+  let pipWindow = await documentPictureInPicture.requestWindow(options);
+  embedThis = document.createElement("iframe")
+  embedThis.style = "width:100vw;height:100vh"
+  embedThis.frameborder = 0
+  embedthis.src = url
+  pipWindow.document.body.append(embedThis);
+}
+
+//Inside of the event listener callback function
+if (!("PictureInPictureEvent" in window)) {
+  document.write("Your browser does not support picture-in-picture. Therefore, Pippy will not work. Please upgrade your browser or contact us at pippyError@foxsden.is-a.dev if you think this is a mistake.")
+}
+if (!("documentPictureInPicture" in window)) {
+  alert("Document Picture-In-Picture is not supported by your browser. Pippy Dock, and interactive webpage will not work.")
+  document.getElementById("requiresDocumentPiP").hidden = true
+}
