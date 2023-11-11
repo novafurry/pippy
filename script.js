cb2499e6413e6605ac50f9da8d6adde89bfb52b2
SamDev <47910472+FoxsDenYT@users.noreply.github.com>
2023-11-11 13:58:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-  let recordingTimeMS = 10000;
-  let preview = document.getElementById("screen");
-  let intsite = document.getElementById("intsite");
-  let startButton = document.getElementById("screenPippy");
-  let pipbtn = document.getElementById("pip");
-  let u2o = document.getElementById("url2open")
-  let anypip = document.getElementById("lpip")
-  var pipWindow;
-  function log(msg) {
+  let preview = document.getElementById("screen"); .// defines a variable for the picture-in-picture's base
+  let intsite = document.getElementById("intsite"); // defines a variable for the interactive website button
+  let startButton = document.getElementById("screenPippy"); // defines a variable for the screen/window/tab button
+  let pipbtn = document.getElementById("pip"); // defines a variable for the show picture-in-picture button
+  let u2o = document.getElementById("url2open") // defines a variable for the url to open textbox in the modal element
+  let anypip = document.getElementById("lpip") // defines a variable for the show pip button in the modal (interactive website)
+  var pipWindow; // globals the pipWindow, defined later
+  function log(msg) { // redirects the log function in the base code to an alert box.
     alert(msg)
   }
-  function customEncode(input) {
+  function customEncode(input) { // custom encoder written by me with the help of GPT.
     // Check if input exists
     if (input) {
       // Convert input to a string
@@ -41,34 +40,31 @@
     }
   }
 
-  function wait(delayInMS) {
+  function wait(delayInMS) { // waits a chosen period of time, in milliseconds
     return new Promise(resolve => setTimeout(resolve, delayInMS));
   }
   function startWebCam(){
-   if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => preview.srcObject = stream).catch(error => log(error));
+   if (navigator.mediaDevices.getUserMedia) { // checks if browser supports UserMedia
+    navigator.mediaDevices.getUserMedia({ video: true }) // asks browser for video userMedia (cameras)
+    .then(stream => preview.srcObject = stream) // sends the users camera to the preview video element
+    .catch(error => log(error)); // logs an error, if one occurs
    }
   }
   async function anySite(url){
-    document.getElementById("urlforPop").textContent = url
-    let options = {
-      width: 400,
-      height: 400,
+    let options = { // defines default size for the interactive PiP
+      width: 400, // measured in pixels (px)
+      height: 400, // measured in pixels (px)
     };
-    pipWindow = await documentPictureInPicture.requestWindow(options);
-    embedThis = document.createElement("div")
-    var codeToInject = 'function customEncode(input) { if (input) { let str = input.toString(); let charArray = str.split(""); let encodedArray = charArray.map((char, index) => { if (index % 2) { return String.fromCharCode(2 ^ char.charCodeAt()); } else { return char; } }); let encodedString = encodedArray.join(""); let finalResult = encodeURIComponent(encodedString); return finalResult; } else { return input; } } var mode = ""; function anErrorOccurred() { if (!(mode == "")) { if (mode == "normal") { i.src = "https://sword.msg.boats/service/" + customEncode(url); mode = "greatsword"; } else { document.write("Sowwyyy 3: We weawwwy twied, but we cawnt embed this site :(<br>please forgive uwu"); } } } url = \'" + url + "\'; i = document.querySelector(\'iframe\'); mode = \'normal\'; i.src = url;';
-
-    embedThis.innerHTML = "<iframe frameborder=0 style='width:100vw;height:100vh;position:absolute;top:0px;left:0px;border:0px solid white;' allowfullscreen src='https://foxsdenyt.github.io/pippy/embedded.html?"+url+"'></iframe>"
-    pipWindow.document.body.append(embedThis);
+    pipWindow = await documentPictureInPicture.requestWindow(options); // requests a documentPictureInPicture (interactive PiP), with the options defined above
+    embedThis = document.createElement("div"); // creates an element, which will be sent to the PiP
+    embedThis.innerHTML = "<iframe frameborder=0 style='width:100vw;height:100vh;position:absolute;top:0px;left:0px;border:0px solid white;' allowfullscreen src='https://foxsdenyt.github.io/pippy/embedded.html?"+url+"'></iframe>" // adds an iframe that loads the embedder page to the afforementioned element.
+    pipWindow.document.body.append(embedThis); // adds the element to the PiP
   }
 
-  //Inside of the event listener callback function
-  if (!("PictureInPictureEvent" in window)) {
+  if (!("PictureInPictureEvent" in window)) { // checks if browser supports PictureInPicture
     document.write("Your browser does not support picture-in-picture. Therefore, Pippy will not work. Please upgrade your browser or contact us at pippyError@foxsden.is-a.dev if you think this is a mistake.")
   }
-  if (!("documentPictureInPicture" in window)) {
+  if (!("documentPictureInPicture" in window)) { // checks if browser supports documentPictureInPicture
     alert("Document Picture-In-Picture is not supported by your browser. Pippy Dock, and interactive webpage will not work.")
     document.getElementById("requiresDocumentPiP").hidden = true
   }
@@ -85,7 +81,7 @@
     .catch(log);
   }, false);
 
-  preview.addEventListener("loadedmetadata", function(){
+  preview.addEventListener("playing", function(){
     wait(1000)
     preview.requestPictureInPicture()
 
